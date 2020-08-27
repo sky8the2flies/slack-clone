a54f400e02abad9b97f170dacce8aea09615e83b
skylerseifert <skylerseifert@gmail.com>
2020-08-27 11:25:28 -0700

    Route for index and root

---
+++ controllers/index.js
@@ -0,0 +1,18 @@
+const Channel = require('../models/channel');
+module.exports = {
+    index
+}
+
+function index(req, res) {
+    Channel.find({}, ).populate('members').exec(function (err, channels) { 
+        if (err) return console.log(err);
+        console.log(channels)
+        res.render('index', { 
+            channel: {
+                current: null,
+                all: channels
+            },
+            user: req.user
+        });
+    });
+}
--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,10 @@
 var express = require('express');
 var router = express.Router();
 const passport = require('passport');
-const Channel = require('../models/channel');
+const indexCtrl = require('../controllers/index');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { 
-    channel: null,
-    user: req.user
-  });
-});
+router.get('/', indexCtrl.index);
 
 router.get('/auth/google', passport.authenticate(
   'google',
