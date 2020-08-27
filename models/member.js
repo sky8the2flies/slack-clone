71b32f75057d51e0df47bd35bce2205b808691a5
skylerseifert <skylerseifert@gmail.com>
2020-08-26 18:37:03 -0700

    Setup dev environment

---
+++ models/member.js
@@ -0,0 +1,12 @@
+var mongoose = require('mongoose');
+
+const memberSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+    avatar: String,
+    googleId: String
+}, {
+    timestamps: true
+});
+
+module.exports = mongoose.model("Member", memberSchema);
--- server.js
+++ server.js
@@ -4,23 +4,42 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var session = require('express-session');
+var passport = require('passport');
+
+require('dotenv').config();
 
 var app = express();
+
+require('./config/database');
+require('./config/passport')
+
+var indexRouter = require('./routes/index');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(session({
+  secret: 'SlaCkClone',
+  resave: false,
+  saveUninitialized: true
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(function(req, res, next) {
+  res.locals.user = req.user;
+  next();
+});
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
