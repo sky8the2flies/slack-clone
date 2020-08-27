6236f9b1e4048ae739bf1f05765e4047f82918c2
skylerseifert <skylerseifert@gmail.com>
2020-08-26 18:36:28 -0700

    Config database

---
+++ config/database.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.DATABASE_URL || `mongodb://localhost/slack-clone`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+});
+
+const db = mongoose.connection;
+
+db.on('connected', function() {
+    console.log(`Mongoose connected to: ${db.host}:${db.port}`)
+});
