e5769e9d3c5dbb7b4751a4dc9df95f38cf0174ee
skylerseifert <skylerseifert@gmail.com>
2020-08-28 22:55:55 -0700

    Add method override

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var methodOverride = require('method-override');
 
 var session = require('express-session');
 var passport = require('passport');
@@ -26,6 +27,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(methodOverride('_method'))
 
 app.use(session({
   secret: 'SlaCkClone',
