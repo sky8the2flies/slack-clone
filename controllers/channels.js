fbace967ec6aa9c7ceba923b73f96c9a1fc82b4a
skylerseifert <skylerseifert@gmail.com>
2020-08-31 15:52:50 -0700

    Add edit channels

--- controllers/channels.js
+++ controllers/channels.js
@@ -1,21 +1,54 @@
 const Channel = require('../models/channel');
 const Message = require('../models/message');
-const Invite = require('../models/invite');
+const Member = require('../models/member');
 
 module.exports = {
     create,
+    edit,
+    update,
     delete: deleteChannel
 }
 
 function create(req, res) {
-    if (!req.user) { return res.redirect('/');}
     req.body.name = req.body.name.trimEnd().trimStart().replace(/\s+/g, '-').replace(/\-+/g, '-').toLowerCase();
     req.body.channelType ? req.body.channelType = '1' : req.body.channelType = '0';
     req.body.members = [req.user._id];
     const channel = new Channel(req.body);
     channel.save(function(err) {
         if (err) console.log(err);
-        res.redirect('/');
+        res.redirect(`/`);
+    });
+}
+
+function edit(req, res) {
+    Channel.find({}, function(err, channels) {
+        if (err) return console.log(err);
+        Channel.findById(req.params.cid, function(err, channel) {
+            if (err || !channel) return res.redierct('/');
+            if (!req.user || !req.user._id.equals(channel.members[0])) return res.redirect(`/channels/${req.params.cid}`);
+            res.render('channels/edit', {
+                channel: {
+                    current: channel,
+                    all: channels
+                },
+                user: req.user
+            });
+        });
+    });
+}
+
+function update(req, res) {
+    req.body.name = req.body.name.trimEnd().trimStart().replace(/\s+/g, '-').replace(/\-+/g, '-').toLowerCase();
+    req.body.channelType ? req.body.channelType = '1' : req.body.channelType = '0';
+    Channel.findById(req.params.cid, function(err, channel) {
+        if (err || !channel) return res.redirect('/');
+        channel.name = req.body.name;
+        channel.description = req.body.description;
+        channel.channelType = req.body.channelType;
+        channel.save(function(err) {
+            if (err) console.log(err);
+            res.redirect(`/channels/${req.params.cid}`);
+        });
     });
 }
 
@@ -26,10 +59,8 @@ function deleteChannel(req, res) {
         channel.remove(function (err) {
             if (err) return console.log(err);
             res.redirect('/');
-            Invite.deleteMany({channel: req.params.cid}, function(err) {
-                Message.deleteMany({channel: req.params.cid}, function(err) {
-                    if (err) return console.log(err);
-                });
+            Message.deleteMany({channel: req.params.cid}, function(err) {
+                if (err) return console.log(err);
             });
         });
     });
--- routes/channels.js
+++ routes/channels.js
@@ -3,18 +3,26 @@ var router = express.Router();
 const channelCtrl = require('../controllers/channels');
 const messageCtrl = require('../controllers/messages');
 const repliesCtrl = require('../controllers/replies');
+const membersCtrl = require('../controllers/members');
 
 router.post('/', channelCtrl.create);
+router.put('/:cid', channelCtrl.update);
 router.delete('/:cid', channelCtrl.delete);
+router.get('/:cid/edit', channelCtrl.edit);
+
 router.get('/:cid', messageCtrl.show);
 router.post('/:cid/messages', messageCtrl.create);
 router.put('/:cid/messages/:mid', messageCtrl.update);
 router.delete('/:cid/messages/:mid', messageCtrl.delete);
 router.get('/:cid/messages/:mid/edit', messageCtrl.edit);
+
 router.get('/:cid/messages/:mid', repliesCtrl.show);
 router.post('/:cid/messages/:mid', repliesCtrl.create);
 router.put('/:cid/messages/:mid/replies/:rid', repliesCtrl.update);
 router.delete('/:cid/messages/:mid/replies/:rid', repliesCtrl.delete);
 router.get('/:cid/messages/:mid/replies/:rid/edit', repliesCtrl.edit);
+
+router.get('/:cid/members/new', membersCtrl.new);
+router.post('/:cid/members', membersCtrl.create);
 
 module.exports = router;
