7bb94984f9a6f7de881b460d1a8f1161b6351471
skylerseifert <skylerseifert@gmail.com>
2020-09-02 11:37:04 -0700

    Deploy to heroku

--- config/passport.js
+++ config/passport.js
@@ -5,7 +5,7 @@ const Member = require('../models/member');
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_SECRET,
-    callbackURL: process.env.GOOGLE_CALLBACK
+    callbackURL: process.env.GOOGLE_CALLBACK || process.env.GOOGLE_CALLBACK_GLOBAL
 },
 function(accessToken, refreshToken, profile, cb) {
     // A user has logged in via Google Oauth
